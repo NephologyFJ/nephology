443934998460df3edde96f7692dc30b323cf6452
grubskim <michal.grubski@ts.fujitsu.com>
2017-01-19 15:36:52 +0100

    Initial "Hello World" clean Java commit

---
+++ src/main/java/org/fujitsu/nephology/App.java
@@ -0,0 +1,13 @@
+package org.fujitsu.nephology;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
