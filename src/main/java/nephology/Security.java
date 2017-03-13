42853e0ba2e9dac91cd2c3857753f43712cbc244
victorhrsilva <victorhugo.ramosdasilva@ts.fujitsu.com>
2017-03-13 14:47:18 +0100

    Security hotfix (#56)
    * Added feature to get application.properties from Environment Variables, still issue with Security class for Stormpath
    
    * # Remove bad defined class
    
    # Add Profile Tags in pom
    
    # Add proxy fields templates in properties
    
    # Add Logger method to Security class
    
    * # Encrypted Travis secure tokens for Heroku and SonarQube
    
    * # Test of SonarQube and SauceLab as Env Variables
    
    * # Test of SonarQube and SauceLab as Env Variables #2
    
    * Removed sauce from travis
    
    * Update .travis.yml
    
    * Merge branch 'master' into security-hotfix
    
    * # Resolved conflicts with master/rebase and removed the additional spaces requested in the review.

--- src/main/java/nephology/Security.java
+++ src/main/java/nephology/Security.java
@@ -1,8 +1,11 @@
 package nephology;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
 
@@ -11,6 +14,9 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
  */
 @Configuration
 public class Security extends WebSecurityConfigurerAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(Security.class);
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.apply(stormpath());
--- src/main/webapp/public/app2.js
+++ src/main/webapp/public/app2.js
@@ -1,7 +1,48 @@
+<script type="text/babel">
 var HelloInput = React.createClass({
+
+   submit: function (e){
+   var self
+
+   e.preventDefault()
+   self = this
+
+   var data = {
+    userName: this.state.userName,
+    message: this.state.message
+   }
+
+   var self = this;
+   $.ajax({
+    url: "/api/helloes",
+    type: 'POST',
+    data: data,
+    })
+    .done(function(data) {
+        self.clearForm()
+    })
+  },
+
+  componentDidMount: function () {
+    this.setState({
+         userName: document.getElementById('userName').value,
+         message: document.getElementById('message').value
+      });
+  },
+
   render: function() {
     return (
-    <div>second component</div>
+        <div className="toast-container div rtl">
+        <form onSubmit={this.submit}>
+            <label class="toast-message">Your Name:&emsp;
+                <input type="text" name="userName" class="toast-message" id='userName'/>
+            </label><br />
+            <label class="toast-message">Message:&emsp;
+                <input type="text" name="message" id='message'/>
+            </label><br />
+            <input type="submit" id='saveName' className="btn btn-info" value="Submit"/>
+        </form>
+        </div>
     );
   }
 });
@@ -9,3 +50,4 @@ var HelloInput = React.createClass({
 ReactDOM.render(
   <HelloInput />, document.getElementById('root2')
 );
+</script>
