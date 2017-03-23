980f3590c95cef09a54c2d92f32ec764482ab4aa
Denis Poisson <denis.poisson@ts.fujitsu.com>
2017-03-23 10:36:34 +0100

    Selenium tests working locally (#61)
    * evironment variables in .travis.yml
    
    * Delete interpreter_config.json
    
    * Delete test_resources_static_index.json
    
    * Delete test_resources_static_index.java
    
    * selenium launched in maven
    
    * selenium launched in maven
    
    * selenium launched in maven
    
    * selenium launched in maven
    
    * selenium launched in maven
    
    * Merge branch 'master' into selenium-enabled-in-maven
    
    # Conflicts:
    #	.travis.yml
    #	pom.xml
    
    * selenium launched in maven
    
    * selenium launched in maven
    
    * selenium launched in maven

---
+++ src/test/nephology/webtest/TestResourcesStaticIndex.java
@@ -0,0 +1,47 @@
+package nephology.webtest;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestResourcesStaticIndex {
+
+    private static HtmlUnitDriver driver;
+    private static StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        driver = new HtmlUnitDriver(true);
+        String target = "https://nephology-test.herokuapp.com";
+
+        driver.navigate().to(target);
+
+        System.out.println("Goto " + target);
+    }
+
+    @Test
+    public void testResourcesStaticIndex() throws Exception {
+
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        wait.until(ExpectedConditions.titleContains("Login"));
+        System.out.println(driver.getCurrentUrl());
+        // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+        assertEquals("Login", driver.getTitle());
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.close();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            Assert.fail(verificationErrorString);
+        }
+    }
+}
