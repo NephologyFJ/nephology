0652b69da8e6796e85e67016b7e006a24cc6252a
digitalfishpond <dfpoisson@gmail.com>
2017-04-23 22:30:58 +0200

    start to build frontend

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InstancesComponent } from './instances/instances.component';
+import { InstanceComponent } from './instance/instance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstancesComponent,
+    InstanceComponent
   ],
   imports: [
     BrowserModule,
